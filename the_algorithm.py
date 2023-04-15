dd92e6442d456fe27a310312e4f8ba6624000236
Piop2 <seungyounyou0220@gmail.com>
2023-04-15 18:31:03 +0900

    add cost system

--- the_algorithm.py
+++ the_algorithm.py
@@ -1,7 +1,7 @@
 """"
 author: Piop2
 
-path finging algorithm for self-driving car navigation system
+path finding algorithm for self-driving car's navigation system
 
 TEST MAP
 #####################
@@ -13,31 +13,63 @@ TEST MAP
 
 
 def path_search(
-    start: int, 
-    end: int, 
-    map_tree: dict[int:list[int]], 
-    path: list[int] = []
+        start: int, end: int, map_nodes: dict[int: list[int]], cost_map: dict[str: int]
+) -> list[tuple[list[int], int]]:
+    return _cost_sort(
+        _search_path(target=end, path=[start], map_nodes=map_nodes), cost_map=cost_map
+    )
+
+
+def _search_path(
+        target: int, path: list[int], map_nodes: dict[int: list[int]]
 ) -> list[list[int]]:
-    if not path:
-        path = [start]
-    next_nodes = list(set(map_tree[path[-1]]) - set(path))
+    if path[0] == target:
+        return [path]
+
+    next_nodes = list(set(map_nodes[path[-1]]) - set(path))
     if not next_nodes:
         return [[]]
 
     all_paths = []
     for node in next_nodes:
         new_path = path + [node]
-        if node == end:
+        if node == target:
             all_paths.append(new_path)
             break
 
-        for next_path in path_search(start, end, map_tree, new_path):
+        for next_path in _search_path(
+                target=target, path=new_path, map_nodes=map_nodes
+        ):
             if not next_path:
                 continue
 
             all_paths.append(next_path)
 
     return all_paths
+
+
+def _cost_sort(paths: list[list[int]], cost_map: dict[str:int]):
+    def get_path_cost(path: list[int]) -> int:
+        # result = 0
+        # for i in range(0, len(path) - 2):
+        #     result += dict[f"{path[i]}-{path[i + 1]}"]
+        # return result
+        return sum(
+            map(
+                lambda x: cost_map[_get_cost_map_key(node1=path[x], node2=path[x + 1])],
+                range(0, len(path) - 2),
+            )
+        )
+
+    path_cost = list(map(lambda x: (x, get_path_cost(x)), paths))
+
+    return sorted(path_cost, key=lambda x: x[1])
+
+
+def _get_cost_map_key(node1: int, node2: int) -> str:
+    if node1 > node2:
+        return f"{node2}-{node1}"
+    return f"{node1}-{node2}"
 
 
 if __name__ == "__main__":
@@ -55,6 +87,21 @@ if __name__ == "__main__":
         11: [10, 12],
         12: [11, 1],
     }
-    routes = path_search(start=9, end=4, map_tree=test_map)
-    sorted_routes = sorted(routes, key=len)
-    print(sorted_routes)
+
+    test_cost = {
+        "1-2": 1,
+        "2-3": 1,
+        "3-4": 1,
+        "4-5": 1,
+        "5-6": 1,
+        "6-7": 1,
+        "7-8": 1,
+        "8-9": 1,
+        "9-10": 1,
+        "10-11": 1,
+        "11-12": 1,
+        "1-12": 1,
+    }
+
+    routes = path_search(start=9, end=4, map_nodes=test_map, cost_map=test_cost)
+    print(routes)
